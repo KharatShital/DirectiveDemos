aece49073bbcf32d2924a55e2e2001ceadd47610
CA-SHITALKHARAT <kharatshital59@gmail.com>
2023-11-30 17:52:45 +0530

    DirectiveDemo Added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'DirectiveDemo';
+  // title = 'DirectiveDemo';
+  showSecret=false;
+  log:any=[]
+
+  onToggleDetails(){
+    this.showSecret =!this.showSecret;
+    this.log.push(this.log.length + 1)
+  }
 }
